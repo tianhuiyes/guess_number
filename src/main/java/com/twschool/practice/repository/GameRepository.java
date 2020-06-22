241b3b221d3dccc5c0618a8cdafdcf1b46792902
huihui <huihui@example.com>
2020-06-22 18:22:31 +0800

    add GameRepository

---
+++ src/main/java/com/twschool/practice/repository/GameRepository.java
@@ -0,0 +1,16 @@
+package com.twschool.practice.repository;
+
+import com.twschool.practice.domain.AnswerGenerator;
+import com.twschool.practice.domain.GuessNumberGame;
+
+public class GameRepository {
+    private GuessNumberGame guessNumberGame;
+    public GuessNumberGame create() {
+        guessNumberGame = new GuessNumberGame(new AnswerGenerator());
+        return guessNumberGame;
+    }
+
+    public GuessNumberGame find() {
+        return guessNumberGame;
+    }
+}
